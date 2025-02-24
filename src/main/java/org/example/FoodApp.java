18d1ac1ad617fda6614f68cc872534b23ff39d8e
ADAMOKEEFFE-STUDENT <G00434897@atu.ie>
2025-02-24 17:43:08 +0000

    food app fries fixed

---
+++ src/main/java/org/example/FoodApp.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FoodApp {
+    public static void main(String[] args) {
+        ArrayList<Food> items = new ArrayList<>();
+        System.out.println("Welcome to Just App");
+        System.out.println("You Order....");
+        //Add some items burgers, pizza, fries and salad...
+        Burger cheeseBurger = new Burger("cheeseBurger",1.99, "Tasty");
+        Pizza Chicken = new Pizza("Pizza",6.99,"Make the right choice");
+        Fries curry = new Fries("Curry",3.50,"Not Healthy go to the gym");
+
+        items.add(cheeseBurger);
+        items.add(Chicken);
+
+
+        for(Food item : items){
+
+            System.out.println(item.getName());
+            System.out.println(item.getPrice());
+            System.out.println(item.getDescription());
+        }
+    }
+}
