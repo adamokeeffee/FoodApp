17623ff24cdb3e9cc3c14c8f9a9c1a624c198133
ADAMOKEEFFE-STUDENT <G00434897@atu.ie>
2025-02-24 17:30:04 +0000

    Burger done

--- src/main/java/org/example/Burger.java
+++ src/main/java/org/example/Burger.java
@@ -1,4 +1,43 @@
 package org.example;
 
-public class Burger {
+public class Burger implements Food {
+
+    String Name;
+
+    double Price;
+
+    String Description;
+
+    public Burger(String name, double price, String description) {
+        this.Name = name;
+        this.Price = price;
+        this.Description = description;
+    }
+
+    @Override
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    @Override
+    public double getPrice() {
+        return Price;
+    }
+
+    public void setPrice(double price) {
+        Price = price;
+    }
+
+    @Override
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
 }
